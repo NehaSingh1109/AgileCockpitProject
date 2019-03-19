db978fa1455e11232e1c26b0143649cc560996c7
NehaSingh1109 <nsnehasingh670@gmail.com>
2019-03-19 21:31:26 +0530

    Updated the xpath and included the display code

--- src/main/java/AmazonTestID/AgileCockpit/AppleWatch.java
+++ src/main/java/AmazonTestID/AgileCockpit/AppleWatch.java
@@ -5,36 +5,28 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+public class AppleWatch extends BaseClass {
 
+	public static WebDriver driver;
+	BaseClass b = new BaseClass();
 
-public class AppleWatch extends BaseClass{
-		
- public static WebDriver driver;
- BaseClass b= new BaseClass();
 	@BeforeMethod
-	public  void setupApplication()
-	{
-		
+	public void setupApplication() {
 		b.getUrl("https://www.amazon.in/");
-		}
-	
+		driver.manage().window().maximize();
+	}
+
 	@Test
-	public void excution() {
-			b.type("Apple watch");
-			b.butt();//click action
-			b.getpart(7);
-			b.rating(10);
-		
+	public void execution() {
+		b.type("Apple watch");
+		b.butt();// click action
+		b.getpart(6);
+		b.rating(9);
 	}
-	
-	
+
 	@AfterMethod
-	public void closeApplication()
-	{
+	public void closeApplication() {
 		driver.quit();
-		
-		
 	}
-	
-	
+
 }
--- src/main/java/AmazonTestID/AgileCockpit/BaseClass.java
+++ src/main/java/AmazonTestID/AgileCockpit/BaseClass.java
@@ -8,55 +8,65 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseClass {
-	public void getpart(int product ) {
+	public void getpart(int product) {
 		// TODO Auto-generated method stub
-		List<WebElement> itemList = AppleWatch.driver.findElements(By.xpath("//a/h2[contains(@class,'s-access-title')]"));
-		
-for (int i = 0; i < itemList.size(); i++) {
-			
-			if(i==product) {
-				System.out.println(itemList.get(i).getText());
-				List<WebElement> price = AppleWatch.driver.findElements(By.xpath("//div[@class='a-fixed-left-grid-col a-col-right']//div//div//span[@class='a-size-base a-color-price s-price a-text-bold']"));
-				for (int j = i; j < price.size(); j++) {
-						if(j==product) {
-							System.out.println(price.get(j).getText());
-							break;
-						}
-				}
-				
-				
-			}
-	}
-	
-}
-	
-	public void rating(int product) {
-		 List<WebElement> rating = AppleWatch.driver.findElements(By.xpath("((//div[@class='a-fixed-left-grid-col a-col-right'])[3]//a[@class='a-size-small a-link-normal a-text-normal'])[1]"));
-		
-		for (int j = 0; j < rating.size(); j++) {
-				if(j==product) {
-					System.out.println(rating.get(j).getText());
-					break;
-				}
+		List<WebElement> itemList = AppleWatch.driver.findElements(By.xpath("//*[contains(@data-attribute,'Apple')]"));
+		for (WebElement Item : itemList) {
+			System.out.println("List items are " + Item.getText());
 		}
+
+		for (int i = 0; i < itemList.size(); i++) {
+
+			if (i == product) {
+				System.out.println(itemList.get(i).getText());
+
+				List<WebElement> price = AppleWatch.driver.findElements(By.xpath(
+						"//div[@class='a-fixed-left-grid-col a-col-right']//div//div//span[@class='a-size-base a-color-price s-price a-text-bold']"));
+				for (int j = i; j < price.size(); j++) {
+					if (j == product) {
+						System.out.println(price.get(j).getText());
+						break;
+					}
+				}
+
 			}
+		}
 
-public void getUrl(String URL) {
-	System.setProperty("webdriver.chrome.driver",
-			"C:\\Users\\nsneh\\Downloads\\chromedriver_win32\\chromedriver.exe");
-	AppleWatch.driver = new ChromeDriver();
-	AppleWatch.driver.get(URL);
-	AppleWatch.driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-}
+	}
 
-public void type( String Data) {
-	WebElement element = AppleWatch.driver.findElement(By.id("twotabsearchtextbox"));
-			element.sendKeys(Data);
-	
-}
-public void butt() {
-	WebElement element =AppleWatch.driver.findElement(By.xpath(".//*[@value='Go']"));
-			element.click();
-}
+	public void rating(int product) {
+		List<WebElement> rating = AppleWatch.driver
+				.findElements(By.xpath("//*[contains(@class,'a-size-small a-link-normal a-text-normal')]"));
+
+		for (WebElement R : rating) {
+			System.out.println("List rating are" + R.getText());
+		}
+
+		for (int j = 0; j < rating.size(); j++) {
+			if (j == product) {
+				System.out.println("Rating is =   " + rating.get(j).getText());
+				break;
+			}
+		}
+	}
+
+	public void getUrl(String URL) {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\nsneh\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		AppleWatch.driver = new ChromeDriver();
+		AppleWatch.driver.get(URL);
+		AppleWatch.driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	}
+
+	public void type(String Data) {
+		WebElement element = AppleWatch.driver.findElement(By.id("twotabsearchtextbox"));
+		element.sendKeys(Data);
+
+	}
+
+	public void butt() {
+		WebElement element = AppleWatch.driver.findElement(By.xpath(".//*[@value='Go']"));
+		element.click();
+	}
 
 }
